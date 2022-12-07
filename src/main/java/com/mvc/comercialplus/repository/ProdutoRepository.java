2b442e0d71f6bdf1bcade18130e1d6cf65aef61e
Murilo Vieira <mvc.dev7cf@gmail.com>
2022-12-07 16:30:35 -0500

    metodos de validacao em service e add de query em repo

--- src/main/java/com/mvc/comercialplus/repository/ProdutoRepository.java
+++ src/main/java/com/mvc/comercialplus/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mvc.comercialplus.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
+	Produto findByCodigoBarras(String codBarras);
 }
--- src/main/java/com/mvc/comercialplus/service/ProdutoService.java
+++ src/main/java/com/mvc/comercialplus/service/ProdutoService.java
@@ -35,5 +35,34 @@ public class ProdutoService implements BdService<Produto>{
 	public void delete(Long id) {
 		repo.deleteById(id);
 	}
+	
+	public Produto getByCodigoBarras(String codBarras) {
+		return repo.findByCodigoBarras(codBarras);
+	}
+	
+	/**
+	 *  Retirado e adaptado do StackoOverflow,
+	 *  respondida 7/03/2016 as 3:17.
+	 *  Disponivel em:
+	 *  https://pt.stackoverflow.com/questions/116712/como-validar-um-c%C3%B3digo-de-barras-padr%C3%A3o-ean-13
+	 *  
+	 *  @author utluiz
+	 */
+	public boolean validarEAN13(String codBarras) {
+        
+		//expressao regular para checar se o codigo e numerico
+		//e tem 13 digitos
+		if (!codBarras.matches("^[0-9]{13}$")) {
+            return false;
+        }
+		
+        int[] numeros = codBarras.chars().map(Character::getNumericValue).toArray();
+        int somaPares = numeros[1] + numeros[3] + numeros[5] + numeros[7] + numeros[9] + numeros[11];
+        int somaImpares = numeros[0] + numeros[2] + numeros[4] + numeros[6] + numeros[8] + numeros[10];
+        int resultado = somaImpares + somaPares * 3;
+        int digitoVerificador = 10 - resultado % 10;
+        
+        return digitoVerificador == numeros[12];
+	}
 
 }
