ab94c5875aff83c36cee321aa1a33181f5437c67
Murilo Vieira <mvc.dev7cf@gmail.com>
2022-12-07 16:31:12 -0500

    paineis faltantes no menu principal

--- src/main/java/com/mvc/comercialplus/GerenciamentoComercialSpringApplication.java
+++ src/main/java/com/mvc/comercialplus/GerenciamentoComercialSpringApplication.java
@@ -4,20 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Rectangle;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -27,29 +25,27 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.UIManager.LookAndFeelInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import com.mvc.comercialplus.gui.Visualizacao;
-import com.mvc.comercialplus.model.Cliente;
-import com.mvc.comercialplus.repository.ClienteRepository;
+import com.mvc.comercialplus.gui.WrapLayout;
+import com.mvc.comercialplus.model.Produto;
+import com.mvc.comercialplus.service.ProdutoService;
 
 @SpringBootApplication
 public class GerenciamentoComercialSpringApplication {
 
-	ClienteRepository repo;
+	ProdutoService produtoService;
 	
 	@Autowired
-	public GerenciamentoComercialSpringApplication(ClienteRepository repo) {
-		this.repo = repo;
+	public GerenciamentoComercialSpringApplication(ProdutoService repo) {
+		this.produtoService = repo;
 		
 		FlatDarculaLaf.setup();
 		
@@ -63,21 +59,19 @@ public class GerenciamentoComercialSpringApplication {
 	
 	static class MenuPrincipal extends JFrame{
 		
-		private JTextArea areaLembrete;
-		
 		private JButton btPagamento;
 		
 		private JTable tabela;
 		
 		private JScrollPane jcp;
 		
-		public ClienteRepository repo;
+		public ProdutoService service;
 		
 		
 		
-		public MenuPrincipal(ClienteRepository repo) {
+		public MenuPrincipal(ProdutoService repo) {
 			
-			this.repo = repo;
+			this.service = repo;
 			/* Como este é o primeiro menu, precisa
 			 * explicitamente ser tornado visível,
 			 * já aque a o método da superclasse 
@@ -90,11 +84,10 @@ public class GerenciamentoComercialSpringApplication {
 			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 			this.setPreferredSize(new Dimension(1280,736));
 			this.setExtendedState(MAXIMIZED_BOTH);
-			this.pack();
+			//this.pack();
 			
 			this.adicionarComponentes();
-			
-			//MenuTurma mt = new MenuTurma(this);
+
 		}
 		
 		
@@ -107,14 +100,16 @@ public class GerenciamentoComercialSpringApplication {
 			try {
 				imagem = Files.readAllBytes(Paths.get("teste.gif"));
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			var iconeChorro = new ImageIcon(imagem);
+			var chorroOriginal = iconeChorro.getImage();
+			iconeChorro.setImage(chorroOriginal.getScaledInstance(70, 100, Image.SCALE_DEFAULT));
 			
-			JLabel lbCategoria = new JLabel(new ImageIcon(imagem));
+			var lbCategoria = new JLabel(iconeChorro);
 			//labelCategoria.setBounds(new Rectangle(new Dimension(220,298)));
-			JLabel lbCodBarras = new JLabel("Código de barras do produto atual");
-			JLabel lbDesconto = new JLabel("Desconto em R$");
+			var lbCodBarras = new JLabel("Código de barras do produto atual");
+			var lbDesconto = new JLabel("Desconto em R$");
 			
 			
 			JPanel pLeteralEsq = new JPanel(new FlowLayout());
@@ -126,48 +121,123 @@ public class GerenciamentoComercialSpringApplication {
 			pLeteralEsq.add(lbDesconto);
 			
 			
+			/*PARTE SUPERIOR DA JANELA*/
+		
+			var campoNomeProduto = new JTextField("Produto Z");
+			campoNomeProduto.setEditable(false);
+			campoNomeProduto.setFont(getFont().deriveFont(36f));
+			campoNomeProduto.setPreferredSize(new Dimension(650,50));
+			campoNomeProduto.setFocusable(false);
+			//campoNomeProduto.addKeyListener(new EscutadorTeclado());
+			var lbQuantidade = new JLabel("X");
+			lbQuantidade.setPreferredSize(new Dimension(15,50));
+			var campoQuantidade = new JTextField("1");
+			campoQuantidade.setFont(getFont().deriveFont(32f));
+			campoQuantidade.setEditable(false);
+			campoQuantidade.setFocusable(false);
+			var lbUnidade = new JLabel("UN.");
+			lbUnidade.setPreferredSize(new Dimension(20,50));
 			
-
+			byte[] arquivoSeta = null;
+			try {
+				arquivoSeta = Files.readAllBytes(Paths.get("right-arrow.png"));
+			} catch (IOException e) {
+				e.getCause().getMessage();
+			}
+			var iconSeta = new ImageIcon(arquivoSeta);
+			var imgParaConverter = iconSeta.getImage();
+			iconSeta.setImage(imgParaConverter.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+			var lbImgSeta = new JLabel(iconSeta);
+			lbImgSeta.setPreferredSize(new Dimension(50,50));
+			var lbPreco = new JLabel("PREÇO:");
+			lbPreco.setFont(getFont().deriveFont(Font.BOLD).deriveFont(36f));
+			//lbPreco.setPreferredSize(new Dimension(100,50));
+			var campoPreco = new JTextField("R$ 2,90");
+			campoPreco.setEditable(false);
+			campoPreco.setFont(getFont().deriveFont(36f));
+			campoPreco.setPreferredSize(new Dimension(200,50));
+			campoPreco.setFocusable(false);
+			
+			var pSegundaLinha = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+			pSegundaLinha.setPreferredSize(new Dimension(700,60));
+			pSegundaLinha.add(lbImgSeta);
+			pSegundaLinha.add(lbPreco);
+			pSegundaLinha.add(campoPreco);
+			
+			JPanel pSuperior = new JPanel(new WrapLayout());
+			pSuperior.setPreferredSize(new Dimension(950,120));
+			pSuperior.add(campoNomeProduto);
+			pSuperior.add(lbQuantidade);
+			pSuperior.add(campoQuantidade);
+			pSuperior.add(lbUnidade);
+			pSuperior.add(pSegundaLinha);
+			
 			
 			/*CENTRO DA JANELA*/
 			
-			List<Cliente> clientes = repo.findAll();
-			Class<?>[] classes = {Long.class, String.class, String.class, String.class, String.class};
+			List<Produto> clientes = service.getAll();
+			Class<?>[] classes = {Long.class, String.class, String.class,
+					String.class, BigDecimal.class, BigDecimal.class};
 			
-			Visualizacao<Cliente> visualizacao = new Visualizacao<Cliente>(
+			Visualizacao<Produto> visualizacao = new Visualizacao<Produto>(
 					clientes,
-					new String[] {"id","email","endereco","telefone","cpf"},
+					new String[] {"id","categoria","codigoBarras","nome","preco", "desconto"},
 					classes,
 					clientes.size(),
-					5);
+					6);
 			
 			tabela = visualizacao.getTable();
 			tabela.addMouseListener(new HabilitarEdicaoExclusao());
 			jcp = new JScrollPane(tabela);
 			
-			btPagamento = new JButton(new AcaoVerTurma());
+			var lbTituloTabela = new JLabel("Produtos adicionados:");
+			lbTituloTabela.setFont(getFont().deriveFont(25f).deriveFont(Font.BOLD));
 			
-			areaLembrete = new JTextArea("Lembrete sobre a turma selecionada...");
-			areaLembrete.setPreferredSize(new Dimension(800, 60));
-			areaLembrete.setFont(new Font("Arial", Font.BOLD+Font.ITALIC, 20));
+			JPanel pCentral = new JPanel(new BorderLayout());
+			pCentral.add(jcp, BorderLayout.CENTER);
+			pCentral.add(lbTituloTabela, BorderLayout.NORTH);
+			
+			
+			/* PARTE INFERIOR DA JANELA */
+			
+			var lbTotal = new JLabel("TOTAL:");
+			lbTotal.setFont(getFont().deriveFont(Font.BOLD).deriveFont(36f));
+			var campoTotal = new JTextField("R$ 6,00");
+			campoTotal.setFont(getFont().deriveFont(Font.BOLD).deriveFont(36f));
+			campoTotal.setEditable(false);
+			campoTotal.setFocusable(false);
+			campoTotal.setPreferredSize(new Dimension(180, 50));
+			
+			btPagamento = new JButton(new AcaoPagamento());
+			btPagamento.setPreferredSize(new Dimension(120,50));
+			
+			JPanel pInferior = new JPanel(new WrapLayout());
+			pInferior.add(lbTotal);
+			pInferior.add(campoTotal);
+			pInferior.add(btPagamento);
 
 			
-			/*ADICAO DOS COMPONENTES NA JANELA*/
+			/*ADICAO DOS PAINEIS NA JANELA*/
 			this.add(pLeteralEsq, BorderLayout.WEST);
-			this.add(jcp, BorderLayout.CENTER);
-			this.add(areaLembrete, BorderLayout.SOUTH);
+			this.add(pSuperior, BorderLayout.NORTH);
+			this.add(pCentral, BorderLayout.CENTER);
+			this.add(pInferior, BorderLayout.SOUTH);
 			
+			this.addKeyListener(new EscutadorTeclado());
+			this.setFocusable(true);
+			this.setAutoRequestFocus(true);
+			this.requestFocusInWindow();
 			
-			desativarBotoes();
+//			desativarBotoes();
 		}
 		
-		private void desativarBotoes() {
-			btPagamento.setEnabled(false);
-		}
-		
-		private void ativarBotoes() {
-			btPagamento.setEnabled(true);
-		}
+//		private void desativarBotoes() {
+//			btPagamento.setEnabled(false);
+//		}
+//		
+//		private void ativarBotoes() {
+//			btPagamento.setEnabled(true);
+//		}
 		
 		protected class HabilitarEdicaoExclusao extends MouseAdapter{
 			
@@ -177,7 +247,7 @@ public class GerenciamentoComercialSpringApplication {
 					/*Quero pegar o Id (está na coluna 0) da linha selecionada*/				
 					long idSelecionado = (long)tabela.getValueAt(tabela.getSelectedRow(), 0);
 					System.out.println(tabela.getSelectedRow());
-					ativarBotoes();
+					//ativarBotoes();
 				
 				}
 
@@ -185,10 +255,34 @@ public class GerenciamentoComercialSpringApplication {
 
 		}
 		
-		protected class AcaoVerTurma extends AbstractAction {
+		
+		
+		protected class EscutadorTeclado extends KeyAdapter {
+			
+			//buffer de caracteres com tamanho maximo suficiente
+			//para acomodar um codigo de barras padrao EAN-13
+			char[] buffer = new char[13];
+			short contador = 0;
+			
+			public void keyTyped(KeyEvent e) {
+				buffer[contador] = e.getKeyChar();
+				contador++;
+				
+				if(contador == 13) {
+					var codBarras = new String(buffer);
+					if(service.validarEAN13(codBarras)) {
+						Produto p = service.getByCodigoBarras(codBarras);
+						//adicionar esse produto na lista
+					}
+					contador = 0;
+				}
+			}
+		}
+		
+		protected class AcaoPagamento extends AbstractAction {
 
-			public AcaoVerTurma() {
-				super("Ver turma");
+			public AcaoPagamento() {
+				super("PAGAMENTO");
 				putValue(MNEMONIC_KEY, KeyEvent.VK_T);
 				putValue(SHORT_DESCRIPTION, "Visualizar turma selecionada");
 			}
@@ -197,7 +291,7 @@ public class GerenciamentoComercialSpringApplication {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				desativarBotoes();
+				//desativarBotoes();
 				
 				long valor = Long.valueOf(String.valueOf(tabela.getValueAt(tabela.getSelectedRow(), 0)));
 				
@@ -205,103 +299,6 @@ public class GerenciamentoComercialSpringApplication {
 			}
 			
 		}
-		
-		protected class AcaoNovaTurma extends AbstractAction {
-
-			
-			public AcaoNovaTurma() {
-				super("Criar nova turma");
-				putValue(MNEMONIC_KEY, KeyEvent.VK_B);
-				putValue(SHORT_DESCRIPTION, "Criar uma nova turma");
-			}
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("AcaoNovaTurma()");
-				
-			}
-			
-		}
-		
-		protected class AcaoEditarTurma extends AbstractAction {
-
-			
-			public AcaoEditarTurma() {
-				super("Editar turma");
-				putValue(MNEMONIC_KEY, KeyEvent.VK_E);
-				putValue(SHORT_DESCRIPTION, "Editar turma selecionada");
-			}
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				long valor = Long.valueOf(String.valueOf(tabela.getValueAt(tabela.getSelectedRow(), 0)));
-				
-				desativarBotoes();
-				
-				System.out.println("AcaoEditarTurma()");
-				
-			}
-			
-		}
-		
-		protected class AcaoApagarTurma extends AbstractAction {
-
-			
-			public AcaoApagarTurma() {
-				super("Apagar turma");
-				putValue(MNEMONIC_KEY, KeyEvent.VK_B);
-				putValue(SHORT_DESCRIPTION, "Apaga a turma selecionada");
-			}
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				desativarBotoes();
-				
-				long valor = Long.valueOf(String.valueOf(tabela.getValueAt(tabela.getSelectedRow(), 0)));
-
-			}
-			
-		}
-		
-		protected class AcaoAbrirMenuAtividades extends AbstractAction {
-			
-			public AcaoAbrirMenuAtividades() {
-				super("Atividades");
-			}
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		}
-		
-		protected class AcaoAbrirMenuAlunos extends AbstractAction {
-			
-			public AcaoAbrirMenuAlunos() {
-				super("Alunos");
-			}
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				
-			}
-		}
-		
-		protected class AcaoAbrirMenuFrequencias extends AbstractAction {
-			
-			public AcaoAbrirMenuFrequencias() {
-				super("Frequências");
-			}
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				
-				
-			}
-		}
-
 
 	}
 	
@@ -344,12 +341,8 @@ public class GerenciamentoComercialSpringApplication {
 		 *podedendo escolher entre diferentes temas */
 	    //FlatLightLaf.setup();
 	    
-		
-
-
 	}
 	
-
 }
 
 
---
+++ src/main/java/com/mvc/comercialplus/controller/CaixaController.java
@@ -0,0 +1,5 @@
+package com.mvc.comercialplus.controller;
+
+public class CaixaController {
+
+}
