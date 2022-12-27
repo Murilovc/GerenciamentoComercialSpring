9e05d5a821715b263695b871705cb06545db84ae
Murilo Vieira <mvc.dev7cf@gmail.com>
2022-12-27 12:29:53 -0500

    feat(CaixaController):metodos para converter BigDecimal em String e vice versa
    Levando em conta o prefixo monetário da aplicação, exemplo, R$

--- src/main/java/com/mvc/comercialplus/controller/CaixaController.java
+++ src/main/java/com/mvc/comercialplus/controller/CaixaController.java
@@ -1,5 +1,20 @@
 package com.mvc.comercialplus.controller;
 
-public class CaixaController {
+import java.math.BigDecimal;
 
+import com.mvc.comercialplus.config.Configuracao;
+
+public class CaixaController {
+	public static BigDecimal converterTextoEmMonetario(String textoCampo) {
+		var semPrefixo = textoCampo.replace(Configuracao.PREFIXO_MONETARIO.get(), "");		
+		var comPonto  = semPrefixo.replace(',', '.');
+		return new BigDecimal(comPonto);
+	}
+	
+	public static String converterMonetarioEmTexto(BigDecimal decimal) {
+		var prefixo = Configuracao.PREFIXO_MONETARIO.get();
+		var textoComVirgula = decimal.toPlainString().replace('.', ',');
+		
+		return prefixo+textoComVirgula;
+	}
 }
