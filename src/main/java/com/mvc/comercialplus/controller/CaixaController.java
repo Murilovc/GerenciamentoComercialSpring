43cf8054767509345a98dd603c67b9288c1d8599
Murilo Vieira <mvc.dev7cf@gmail.com>
2022-12-29 23:22:19 -0500

    feat(CaixaController): metodo para aplicar taxa do cartao

--- src/main/java/com/mvc/comercialplus/controller/CaixaController.java
+++ src/main/java/com/mvc/comercialplus/controller/CaixaController.java
@@ -1,8 +1,10 @@
 package com.mvc.comercialplus.controller;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import com.mvc.comercialplus.config.Configuracao;
+import com.mvc.comercialplus.model.FormaPagamento;
 
 public class CaixaController {
 	public static BigDecimal converterTextoEmMonetario(String textoCampo) {
@@ -16,5 +18,15 @@ public class CaixaController {
 		var textoComVirgula = decimal.toPlainString().replace('.', ',');
 		
 		return prefixo+textoComVirgula;
+	}
+	
+	public static BigDecimal aplicarTaxaCartao(BigDecimal valorProdutos, FormaPagamento tipoCartao) {
+		
+		var taxaEmDecimal = BigDecimal.valueOf(tipoCartao.taxaPorcentagem()).setScale(2, RoundingMode.HALF_EVEN);
+		System.out.println(taxaEmDecimal);
+		var totalComTaxa = valorProdutos.multiply(taxaEmDecimal).setScale(2, RoundingMode.HALF_EVEN);
+		System.out.println(totalComTaxa);
+		
+		return totalComTaxa;
 	}
 }
